e61c06810211d7ee47a8282f2c35a65fa1c4bd77
Lourenz Angel A. Orcales <laorcales@paterostechnologicalcollege.edu.ph>
2026-03-11 16:40:55 +0800

    Update Exercise 3.py

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,1 +1,40 @@
+print("Hello, I'm Lourenz Angel A. Orcales! 1V")
 
+# My first simple script
+print()
+print("Hello, this is my first Python script!")
+
+print("\n---- Python Variables ----")
+name = "Lourenz"
+number = 19
+
+print("Name:", name)
+print("Number:", number)
+
+print("\n---- Python Variable Names ----")
+
+nicole = "nicole"
+mico = "mico"
+marjorie = "marjorie"
+riza = "riza"
+
+print(nicole)
+print(mico)
+print(marjorie)
+print(riza)
+
+print("\n---- Many Values to Multiple Variables ----")
+
+x, y, z = 10, 20, 30
+
+print(x)
+print(y)
+print(z)
+
+print("\n---- One Value to Multiple Variables ----")
+
+a = b = c = 19
+
+print(a)
+print(b)
+print(c)
